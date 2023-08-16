c3d53b0f041bcdaca5bb011c74796708b42b7318
Ramazan Abdurahmanov <r0ma.abdurahmanov93@gmail.com>
2023-08-16 22:26:13 +0400

    Rest Api with @PostMapping and @RequestBody

--- springboot-rest-api/src/main/java/az/ramazan/springbootrestapi/controllers/StudentController.java
+++ springboot-rest-api/src/main/java/az/ramazan/springbootrestapi/controllers/StudentController.java
@@ -1,10 +1,8 @@
 package az.ramazan.springbootrestapi.controllers;
 
 import az.ramazan.springbootrestapi.beans.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,6 +36,14 @@ public class StudentController {
                                            @RequestParam String lastName){
 
         return new Student(id,firstName,lastName);
+    }
+    @PostMapping("students/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student){
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
     }
 
 }
