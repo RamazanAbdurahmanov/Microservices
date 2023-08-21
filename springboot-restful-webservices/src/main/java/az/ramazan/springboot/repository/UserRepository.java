1ab566a39c78c30a5efaefa64d35cddba9357bf4
Ramazan Abdurahmanov <r0ma.abdurahmanov93@gmail.com>
2023-08-21 23:23:49 +0400

    Create Spring Data JPA Repository-UserRepository

---
+++ springboot-restful-webservices/src/main/java/az/ramazan/springboot/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package az.ramazan.springboot.repository;
+
+import az.ramazan.springboot.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
