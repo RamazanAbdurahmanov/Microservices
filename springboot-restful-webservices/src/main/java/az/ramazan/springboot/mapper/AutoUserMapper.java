c6e5a99cb0825923c728a2cdff462c819a236532
Ramazan Abdurahmanov <r0ma.abdurahmanov93@gmail.com>
2023-08-29 22:38:18 +0400

    Using MapStruct Library step 2 Create Mapper

---
+++ springboot-restful-webservices/src/main/java/az/ramazan/springboot/mapper/AutoUserMapper.java
@@ -0,0 +1,13 @@
+package az.ramazan.springboot.mapper;
+
+import az.ramazan.springboot.dto.UserDto;
+import az.ramazan.springboot.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface AutoUserMapper {
+    AutoUserMapper MAPPER= Mappers.getMapper(AutoUserMapper.class);
+    UserDto mapToUserDto(User user);
+    User mapToUser(UserDto userDto);
+}
