d406060483da8872d44966d50d53919f60c0d5a9
Ramazan Abdurahmanov <r0ma.abdurahmanov93@gmail.com>
2023-08-31 00:07:30 +0400

    Handing Specific Custom Exception

--- springboot-restful-webservices/src/main/java/az/ramazan/springboot/controller/UserController.java
+++ springboot-restful-webservices/src/main/java/az/ramazan/springboot/controller/UserController.java
@@ -2,13 +2,17 @@ package az.ramazan.springboot.controller;
 
 import az.ramazan.springboot.dto.UserDto;
 import az.ramazan.springboot.entity.User;
+import az.ramazan.springboot.exception.ErrorDetails;
+import az.ramazan.springboot.exception.ResourceNotFoundException;
 import az.ramazan.springboot.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -52,4 +56,18 @@ public class UserController {
 
 
     }
+//    @ExceptionHandler(ResourceNotFoundException.class)
+//    public ResponseEntity<ErrorDetails>handleResourceNotFoundException(
+//            ResourceNotFoundException exception,
+//            WebRequest webRequest){
+//        ErrorDetails errorDetails= new ErrorDetails(
+//                LocalDateTime.now(),
+//                exception.getMessage(),
+//                webRequest.getDescription(false),
+//                "USER_NOT_FOUND"
+//        );
+//        return new ResponseEntity<>(errorDetails,HttpStatus.NOT_FOUND);
+//
+//
+//    }
 }
