899f4cc0cb7de556c1b3c223c0dba3ef632ebbfb
Ramazan Abdurahmanov <r0ma.abdurahmanov93@gmail.com>
2023-08-30 23:12:04 +0400

    Create and Use Custom Exception ResourceNotFoundException

--- springboot-restful-webservices/src/main/java/az/ramazan/springboot/service/impl/UserServiceImpl.java
+++ springboot-restful-webservices/src/main/java/az/ramazan/springboot/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package az.ramazan.springboot.service.impl;
 
 import az.ramazan.springboot.dto.UserDto;
 import az.ramazan.springboot.entity.User;
+import az.ramazan.springboot.exception.ResourceNotFoundException;
 import az.ramazan.springboot.mapper.AutoUserMapper;
 import az.ramazan.springboot.mapper.UserMapper;
 import az.ramazan.springboot.repository.UserRepository;
@@ -37,11 +38,12 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto getUserById(Long userId) {
-        Optional<User> optionalUser = userRepository.findById(userId);
-        User user = optionalUser.get();
+        User user = userRepository.findById(userId).orElseThrow(
+                ()-> new ResourceNotFoundException("User","id",userId)
+        );
         //return UserMapper.mapToUserDto(user);
         //return modelMapper.map(user, UserDto.class);
-        return AutoUserMapper.MAPPER.mapToUserDto(optionalUser.get());
+        return AutoUserMapper.MAPPER.mapToUserDto(user);
 
     }
 
@@ -58,7 +60,10 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto updateUser(UserDto user) {
-        User existingUser = userRepository.findById(user.getId()).get();
+        User existingUser = userRepository.findById(user.getId()).orElseThrow(
+                ()->new ResourceNotFoundException("User","id",user.getId())
+        );
+
         existingUser.setFirstName(user.getFirstName());
         existingUser.setLastName(user.getLastName());
         existingUser.setEmail(user.getEmail());
@@ -70,6 +75,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void deleteUser(Long userId) {
+        User existingUser = userRepository.findById(userId).orElseThrow(
+                ()->new ResourceNotFoundException("User","id",userId)
+        );
         userRepository.deleteById(userId);
     }
 }
