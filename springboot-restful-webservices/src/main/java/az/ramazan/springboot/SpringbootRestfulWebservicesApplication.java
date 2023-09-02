59f482826d578ec399b912426e31bde21cf00b4c
Ramazan Abdurahmanov <r0ma.abdurahmanov93@gmail.com>
2023-09-02 23:14:22 +0400

    OpenAPIDefinition

--- springboot-restful-webservices/src/main/java/az/ramazan/springboot/SpringbootRestfulWebservicesApplication.java
+++ springboot-restful-webservices/src/main/java/az/ramazan/springboot/SpringbootRestfulWebservicesApplication.java
@@ -1,11 +1,27 @@
 package az.ramazan.springboot;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =@Info(title="Spring Boot Rest API Documentation",
+        description = "Spring Boot Rest API Documentation",
+        version = "v1.0",
+        contact = @Contact(name="Ramazan",email = "ramazan_arsenal@mail.ru",url = "linkedin.com/in/ramazanabdurahmanov"),
+        license = @License(
+                name = "Apache 2.0",
+                url="linkedin.com/in/ramazanabdurahmanov/"
+
+        )
+
+))
 public class SpringbootRestfulWebservicesApplication  {
     @Bean
     public ModelMapper modelMapper() {
