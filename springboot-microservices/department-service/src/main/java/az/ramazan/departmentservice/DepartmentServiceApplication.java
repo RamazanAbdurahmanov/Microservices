68e8305429c7df97797a6438a7d8ca593309eb83
Ramazan Abdurahmanov <r0ma.abdurahmanov93@gmail.com>
2023-09-19 22:00:22 +0400

    Refactor Department-Service to use Config server

--- springboot-microservices/department-service/src/main/java/az/ramazan/departmentservice/DepartmentServiceApplication.java
+++ springboot-microservices/department-service/src/main/java/az/ramazan/departmentservice/DepartmentServiceApplication.java
@@ -2,8 +2,6 @@ package az.ramazan.departmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
 @SpringBootApplication
 public class DepartmentServiceApplication {
 
